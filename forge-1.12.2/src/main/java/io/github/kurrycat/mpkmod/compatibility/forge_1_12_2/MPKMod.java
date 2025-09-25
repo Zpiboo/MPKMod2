5bc95f1b2e41aff2eaa0e473ded8beca1504bb9d
Zpiboo <zpiboo@outlook.com>
2025-09-25 23:39:35 +0200

    feat: copy forge-1.8.9 submodule

---
+++ forge-1.12.2/src/main/java/io/github/kurrycat/mpkmod/compatibility/forge_1_12_2/MPKMod.java
@@ -0,0 +1,87 @@
+package io.github.kurrycat.mpkmod.compatibility.forge_1_12_2;
+
+import io.github.kurrycat.mpkmod.compatibility.API;
+import io.github.kurrycat.mpkmod.compatibility.forge_1_12_2.network.MPKForgeNetworking;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.lwjgl.input.Keyboard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mod(
+        modid = API.MODID,
+        version = API.VERSION,
+        name = API.NAME,
+        acceptedMinecraftVersions = "*"//,
+        //updateJSON = "https://raw.githubusercontent.com/kurrycat2004/MpkMod/main/update.json",
+        //guiFactory = MPKMod.GUI_FACTORY
+)
+public class MPKMod {
+    //public static final String GUI_FACTORY = "io.github.kurrycat.mpkmod.config.GuiFactory";
+
+    public static Map<String, KeyBinding> keyBindingMap = new HashMap<>();
+    public static final MPKForgeNetworking FORGE_NETWORKING = new MPKForgeNetworking();
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        FORGE_NETWORKING.init();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        API.preInit(getClass());
+
+        API.guiScreenMap.forEach((id, guiScreen) -> {
+            if (guiScreen.shouldCreateKeyBind())
+                registerKeyBinding(id);
+        });
+
+        API.keyBindingMap.forEach((id, consumer) -> registerKeyBinding(id));
+
+        API.LOGGER.info(API.COMPATIBILITY_MARKER, "Registering compatibility functions...");
+        API.registerFunctionHolder(new FunctionCompatibility());
+        API.LOGGER.info(API.COMPATIBILITY_MARKER, "Done");
+
+        MinecraftForge.EVENT_BUS.register(new EventListener());
+        MinecraftForge.EVENT_BUS.register(this);
+
+        registerKeyBindings();
+        API.init(Minecraft.getSessionInfo().get("X-Minecraft-Version"));
+    }
+
+    public static void registerKeyBinding(String id) {
+        KeyBinding keyBinding = new KeyBinding(
+                API.MODID + ".key." + id + ".desc",
+                Keyboard.KEY_NONE,
+                API.KEYBINDING_CATEGORY
+        );
+        keyBindingMap.put(id, keyBinding);
+        ClientRegistry.registerKeyBinding(keyBinding);
+    }
+
+    private void registerKeyBindings() {
+        for (KeyBinding k : Minecraft.getMinecraft().gameSettings.keyBindings) {
+            new io.github.kurrycat.mpkmod.compatibility.MCClasses.KeyBinding(
+                    () -> GameSettings.getKeyDisplayString(k.getKeyCode()),
+                    k.getKeyDescription(),
+                    k::isKeyDown
+            );
+        }
+        API.LOGGER.info(API.COMPATIBILITY_MARKER, "Registered {} Keybindings",
+                io.github.kurrycat.mpkmod.compatibility.MCClasses.KeyBinding.getKeyMap().size());
+    }
+
+    @EventHandler
+    public void loadComplete(FMLLoadCompleteEvent e) {
+        API.Events.onLoadComplete();
+    }
+}
