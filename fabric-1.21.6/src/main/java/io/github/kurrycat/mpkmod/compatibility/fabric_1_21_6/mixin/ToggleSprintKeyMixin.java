6e4a203fcc10527497cf959e6eca56f87093debf
Zpiboo <zpiboo@outlook.com>
2026-01-07 15:29:32 +0100

    feat(mixin): handle togglesprint logic in KeyMapping mixin

---
+++ fabric-1.21.6/src/main/java/io/github/kurrycat/mpkmod/compatibility/fabric_1_21_6/mixin/ToggleSprintKeyMixin.java
@@ -0,0 +1,20 @@
+package io.github.kurrycat.mpkmod.compatibility.fabric_1_21_6.mixin;
+import io.github.kurrycat.mpkmod.compatibility.MCClasses.Minecraft;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(KeyBinding.class)
+public class ToggleSprintKeyMixin {
+    @Inject(method = "isPressed", at = @At("HEAD"), cancellable = true)
+    private void sprintOverride(CallbackInfoReturnable<Boolean> cir) {
+        KeyBinding self = (KeyBinding) (Object) this;
+        KeyBinding sprintKey = MinecraftClient.getInstance().options.sprintKey;
+
+        if (self == sprintKey && Minecraft.isSprintToggled())
+            cir.setReturnValue(true);
+    }
+}
