68cb37d9881b2afee397c097f71764d40c408dbe
Zpiboo <zpiboo@outlook.com>
2026-03-03 22:42:29 +0100

    feat(timing): make TimingEntry json-deserializable and mirrorable

--- common/src/main/java/io/github/kurrycat/mpkmod/ticks/TimingEntry.java
+++ common/src/main/java/io/github/kurrycat/mpkmod/ticks/TimingEntry.java
@@ -1,114 +1,97 @@
 package io.github.kurrycat.mpkmod.ticks;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import io.github.kurrycat.mpkmod.util.MathUtil;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.kurrycat.mpkmod.util.Range;
 import io.github.kurrycat.mpkmod.util.Tuple;
+import io.github.kurrycat.mpkmod.util.input.InputPredicate;
+import io.github.kurrycat.mpkmod.util.input.InputPredicateBase;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-public class TimingEntry {
-    private static final String tickCountRegex = "^((((?<diffNumber>\\d+)-)?((?<varName>[a-zA-Z]+)(\\{(?<lowerRange>\\d+)?,(?<upperRange>\\d+)?})?))|(?<rawCount>\\d+))$";
-    private static final Pattern tickCountPattern = Pattern.compile(tickCountRegex);
-    private static final String tickInputRegex = "^W?A?S?D?P?N?J?(!?G)?$";
-    private static final Pattern tickInputPattern = Pattern.compile(tickInputRegex);
+public final class TimingEntry {
+    public final String varName;
+    public final Range range;
 
-    public String timingEntry;
-    public TimingInput timingInput;
-
-    private Integer number;
-    private String varName;
-    private Range range;
+    public final InputPredicateBase inputPredicate;
+    public final Boolean P, N;
+    public final GroundState G;
 
     @JsonCreator
-    public TimingEntry(String timingEntry) {
-        this.timingEntry = timingEntry;
-        String[] split = timingEntry.split(":", -1);
-        if (split.length == 1) {
-            number = 1;
-            varName = null;
-            range = null;
-            timingInput = new TimingInput(split[0]);
-        } else if (split.length == 2) {
-            Matcher matcher = tickCountPattern.matcher(split[0]);
-            if (!matcher.matches())
-                throw new IllegalArgumentException(String.format("Invalid tick count: %s", split[0]));
+    public TimingEntry(
+            @JsonProperty("var")
+            String varName,
+            @JsonProperty("min")
+            Integer min,
+            @JsonProperty("max")
+            Integer max,
+            @JsonProperty("inputs")
+            InputPredicateBase inputPredicate,
+            @JsonProperty("sprint")
+            Boolean P,
+            @JsonProperty("sneak")
+            Boolean N,
+            @JsonProperty("groundState")
+            GroundState G
+    ) {
+        if (min == null && max == null) min = max = 1;
+        if (min == null || min <= 0) min = 0;
+        if (max != null && max < min) max = min;
 
-            Integer diffNumber = MathUtil.parseInt(matcher.group("diffNumber"), null);
-            String varName = matcher.group("varName");
-            Integer lower = MathUtil.parseInt(matcher.group("lowerRange"), null);
-            Integer upper = MathUtil.parseInt(matcher.group("upperRange"), null);
-            Integer rawCount = MathUtil.parseInt(matcher.group("rawCount"), null);
-
-            number = diffNumber == null ? rawCount : diffNumber;
-            this.varName = varName;
-            range = new Range(lower, upper);
-
-            timingInput = new TimingInput(split[1]);
-        } else {
-            throw new IllegalArgumentException(String.format("More than one : found in timingEntry '%s', expected one", timingEntry));
-        }
+        this.varName = varName;
+        this.range = new Range(min, max);
+        this.inputPredicate = inputPredicate;
+        this.P = P;
+        this.N = N;
+        this.G = G;
     }
 
     public boolean varNameMatches(TimingEntry other) {
         return varName != null && other.varName != null && varName.equals(other.varName);
     }
 
+    private boolean matchesInput(TimingInput timingInput) {
+        return (
+                (inputPredicate == null || inputPredicate.matches(timingInput.inputVector)) &&
+                (P == null || P == timingInput.P) &&
+                (N == null || N == timingInput.N) &&
+                (G == null || G == timingInput.G)
+        );
+    }
+
     /**
      * @param inputList List of {@link TimingInput} instances
      * @param startIndex the index it should start matching from
-     * @param vars      HashMap containing all variables for that {@link Timing}. Vars of this TimingEntry are added
-     * @param repeatedVar if the current var already appeared before
+     * @param vars HashMap containing all variables for that {@link Timing}. Vars of this TimingEntry are added
+     * @param repeatedVar if the current var appeared in the previous {@link TimingEntry}
      * @return amount of matched inputs or null if no match was found (returns 0 for variable entries with 0 within range)
      */
     public Integer matches(List<TimingInput> inputList, int startIndex, HashMap<String, Timing.TickMS> vars, boolean repeatedVar) {
-        if (number == null && varName == null) return null;
+        int i = startIndex;
 
-        // is variable
-        if (number == null) {
-            int i = startIndex;
-            while (i < inputList.size() && inputList.get(i).equals(timingInput)) i++;
+        if (inputPredicate instanceof InputPredicate) ((InputPredicate) inputPredicate).resetLastMatch();
+        while (i < inputList.size() && matchesInput(inputList.get(i))) i++;
+        int count = range.constrain(i - startIndex);
 
-            i -= startIndex;
+        if (range.isValueBelow(i - startIndex)) return null;
 
-            if (range.includes(i) || range.isAbove(i)) {
-                if (vars.containsKey(varName) && repeatedVar)
-                    vars.get(varName).tickCount += range.constrain(i);
-                else vars.put(varName, new Timing.TickMS(range.constrain(i)));
-
-                vars.get(varName).ms = getMS(
-                        startIndex + i - vars.get(varName).tickCount,
-                        vars.get(varName).tickCount,
-                        inputList
-                );
-
-                return range.constrain(i);
-            }
-            return null;
-        }
-
-        int countToMatch = number;
         if (varName != null) {
-            if (!vars.containsKey(varName)) {
-                throw new IllegalArgumentException(String.format("The variable %s has not been defined in a prior timing input (at %s)", varName, timingEntry));
+            if (vars.containsKey(varName) && repeatedVar) {
+                vars.get(varName).tickCount += count;
             } else {
-                countToMatch -= vars.get(varName).tickCount;
+                vars.put(varName, new Timing.TickMS(count));
             }
+
+            vars.get(varName).ms = getMS(
+                    i - vars.get(varName).tickCount,
+                    vars.get(varName).tickCount,
+                    inputList
+            );
         }
 
-        if (inputList.size() == startIndex && number != 0) return null;
-
-        int i;
-        for (i = startIndex; countToMatch > 0; countToMatch--, i++) {
-            if (i >= inputList.size()) return null;
-            if (!inputList.get(i).equals(timingInput)) return null;
-        }
-        return i - startIndex;
+        return count;
     }
 
     private Integer getMS(int startIndex, int matchCount, List<TimingInput> inputList) {
@@ -134,5 +117,13 @@ public class TimingEntry {
         ButtonMS endMS = after.msList.forKey(range.getSecond());
         if (endMS == null) return null;
         return endMS.msFrom(startMS);
+    }
+
+    public TimingEntry mirrored() {
+        InputPredicateBase mirroredInputPredicate = inputPredicate;
+        if (inputPredicate instanceof InputPredicate)
+            mirroredInputPredicate = ((InputPredicate) inputPredicate).mirrored();
+
+        return new TimingEntry(varName, range.getLower(), range.getUpper(), mirroredInputPredicate, P, N, G);
     }
 }
