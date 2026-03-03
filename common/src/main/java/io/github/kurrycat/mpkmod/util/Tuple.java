18f67611199ee620830af05c0a9c80913a0a0347
Zpiboo <zpiboo@outlook.com>
2026-03-03 22:30:57 +0100

    feat(api): provide Tuple with equals method

--- common/src/main/java/io/github/kurrycat/mpkmod/util/Tuple.java
+++ common/src/main/java/io/github/kurrycat/mpkmod/util/Tuple.java
@@ -1,5 +1,7 @@
 package io.github.kurrycat.mpkmod.util;
 
+import java.util.Objects;
+
 public class Tuple<A, B> implements Copyable<Tuple<A, B>> {
     private A a;
     private B b;
@@ -26,7 +28,7 @@ public class Tuple<A, B> implements Copyable<Tuple<A, B>> {
     }
 
     @SuppressWarnings("unchecked")
-    public Tuple<A,B> copy() {
+    public Tuple<A, B> copy() {
         return new Tuple<>(
                 this.a instanceof Copyable ? ((Copyable<A>) this.a).copy() : this.a,
                 this.b instanceof Copyable ? ((Copyable<B>) this.b).copy() : this.b
@@ -36,5 +38,14 @@ public class Tuple<A, B> implements Copyable<Tuple<A, B>> {
     @Override
     public String toString() {
         return "Tuple{" + a + ", " + b + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tuple<?, ?> that = (Tuple<?, ?>) o;
+
+        return Objects.equals(a, that.a) && Objects.equals(b, that.b);
     }
 }
